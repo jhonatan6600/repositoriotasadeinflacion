6122a024f9710d87b9ad92942b6f1bdcfd77c271
Jhonatan <Jhonatan@DESKTOP-UORMG4Q>
2023-07-11 20:31:13 -0400

    carga inicial

---
+++ src/cortezcari/Cortezcari.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cortezcari;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Jhonatan
+ */
+public class Cortezcari {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int dato;
+                dato=Integer.parseInt(JOptionPane.showInputDialog("Introduzca el numero del pais"));
+                switch (dato){
+                    case 1 : JOptionPane.showMessageDialog(null,"La taza de inflacion de ARGENTINA es 200.91%");
+                    break;
+                    case 2 : JOptionPane.showMessageDialog(null,"La taza de inflacion de HAITI es 72.43%");
+                    break;
+                    case 3 : JOptionPane.showMessageDialog(null,"La taza de inflacion de SURINAM es 27.58%");
+                    break;
+                    case 4 : JOptionPane.showMessageDialog(null,"La taza de inflacion de COLOMBIA es 52.45%");
+                    break;
+                    case 5 : JOptionPane.showMessageDialog(null,"La taza de inflacion de NICARAGUA es 10.18%");
+                    break;
+                    case 6 : JOptionPane.showMessageDialog(null,"La taza de inflacion de CHILE es 10.45%");
+                    break;
+                    case 7 : JOptionPane.showMessageDialog(null,"La taza de inflacion de URUGUAY es 11.65%");
+                    break;
+                    case 8 : JOptionPane.showMessageDialog(null,"La taza de inflacion de GUATEMALA es 9.11%");
+                    break;
+                    case 9 : JOptionPane.showMessageDialog(null,"La taza de inflacion de JAMAICA es 6.89%");
+                    break;
+                    case 10 : JOptionPane.showMessageDialog(null,"La taza de inflacion de HONDURAS es 9.58%");
+                    break;
+                    case 11 : JOptionPane.showMessageDialog(null,"La taza de inflacion de BARBADOS es 9.09%");
+                    break;
+                    case 12 : JOptionPane.showMessageDialog(null,"La taza de inflacion de GUAYANA es 9.41%");
+                    break;
+                    case 13 : JOptionPane.showMessageDialog(null,"La taza de inflacion de MEXICO es 6.47%");
+                    break;
+                    case 14 : JOptionPane.showMessageDialog(null,"La taza de inflacion de DOMINICA es 7.98%");
+                    break;
+                    case 15 : JOptionPane.showMessageDialog(null,"La taza de inflacion de REPUBLICA DOMINICANA es 7.48%");
+                    break;
+                    case 16 : JOptionPane.showMessageDialog(null,"La taza de inflacion de PERU es 7.87%");
+                    break;
+                    case 17 : JOptionPane.showMessageDialog(null,"La taza de inflacion de TRINIDAD Y TOBAGO es 5.83%");
+                    break;
+                    case 18 : JOptionPane.showMessageDialog(null,"La taza de inflacion de COSTA RICA es 8.28%");
+                    break;
+                    case 19 : JOptionPane.showMessageDialog(null,"La taza de inflacion de PARAGUAY es 9.77%");
+                    break;
+                    case 20 : JOptionPane.showMessageDialog(null,"La taza de inflacion de ANTIGUA Y BARBUDA es 7.53%");
+                    break;
+                    case 21 : JOptionPane.showMessageDialog(null,"La taza de inflacion de BRASIL es 9.28%");
+                    break;
+                    case 22 : JOptionPane.showMessageDialog(null,"La taza de inflacion de SANTA LUCIA es 6.74%");
+                    break;
+                    case 23 : JOptionPane.showMessageDialog(null,"La taza de inflacion de BAHAMAS es 5.61%");
+                    break;
+                    case 24 : JOptionPane.showMessageDialog(null,"La taza de inflacion de BELICE es 6.27%");
+                    break;
+                    case 25 : JOptionPane.showMessageDialog(null,"La taza de inflacion de EL SALVADOR es 7.28%");
+                    break;
+                    case 26 : JOptionPane.showMessageDialog(null,"La taza de inflacion de BOLIVIA es 1.75%");
+                    break;
+                    case 27 : JOptionPane.showMessageDialog(null,"La taza de inflacion de ARUBA es 5.52%");
+                    break;
+                    case 28 : JOptionPane.showMessageDialog(null,"La taza de inflacion de GRANADA es 2.65%");
+                    break;
+                    case 29 : JOptionPane.showMessageDialog(null,"La taza de inflacion de ECUADOR es 3.47%");
+                    break;
+                    case 30 : JOptionPane.showMessageDialog(null,"La taza de inflacion de PANAMA es 2.86%");
+                    break;
+                    default: JOptionPane.showMessageDialog(null,"El numero no esta en la lista introduzca otro");
+                    
+                    
+                }
+    }
+        
+        // TODO code application logic here
+    }
+    
